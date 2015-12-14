3fe3e0b5676d14034d5d3f0638ad3abcb915d96d
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-12-14 15:12:37 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk/test@26031 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- run.js
+++ run.js
@@ -1,4 +1,16 @@
-
+/**
+ *
+ * JavaScript Test Suite
+ * =====================
+ *
+ * Copyright (c) 2015, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/
+ * All rights reserved.
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ *
+ **/
 
 load("test.equality.js");
 //load("test.inequality.js");
