ba426687580f117399a3b3e25d3903f23db71cda
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2015-12-14 15:11:36 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk/test@26030 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- base.js
+++ base.js
@@ -1,6 +1,7 @@
 /**
  *
  * JavaScript Test Suite
+ * =====================
  *
  * Copyright (c) 2015, Proglang, University of Freiburg.
  * http://proglang.informatik.uni-freiburg.de/
@@ -14,13 +15,58 @@
 /**
  * Constructor function for tests
  **/
-function Test(name, test) {
+function Test(name, closure) {
   if(!(this instanceof Test)) return new Test(...Array.from(arguments));
 
   this.name = name;
-  this.test = test;
+  this.closure = closure;
 
   Test.tests.add(this);
+}
+
+/**
+ * Function toBe
+ **/
+Test.prototype.toBe = function (name="unnamed test", skip=false, value=undefined) {
+  if(skip) {
+    Test.skipped.add({name:name, predicate:this});
+    return true;
+  } 
+
+  var result = this.apply();
+
+  if(result===value) {
+    Test.passed.add({name:name, predicate:this});
+    return true;
+  } else {
+    Test.failed.add({name:name, predicate:this, expect:value, given:result});
+    return true;
+  }
+}
+
+/**
+ * Function expect 
+ **/
+Test.prototype.expect = function(predicate, skip=false) {
+  Object.defineProperty(predicate, "toBe", {
+    value:Test.prototype.toBe.bind(predicate, this.name, skip), writable: false, configurable: false
+  });
+
+  return predicate;
+}
+
+/**
+ * Function expect true
+ **/
+Test.expectTrue = function (predicate, skip=false) {
+  return Test.prototype.toBe.apply(predicate, this.name, skip, true);
+}
+
+/**
+ * Function expect false
+ **/
+Test.expectFalse = function (predicate, skip=false) {
+  return Test.prototype.toBe.apply(predicate, this.name, skip, false);
 }
 
 /**
@@ -31,9 +77,9 @@ Test.tests = new Set();
 /**
  * Lists of all test cases
  **/
-Test.failed = new Set();
+Test.failed  = new Set();
 Test.skipped = new Set();
-Test.passed = new Set();
+Test.passed  = new Set();
 
 /**
  * Runs the created test tests 
@@ -42,10 +88,10 @@ Test.run = function(verbose=false, ion=false, baseline=false) {
   var tstart = Date.now();
 
   for(var test of Test.tests) {
-    if(verbose) print(`\n;;; Run ${test.name}`);
-    test.test.apply(test);
+    if(verbose) print(`\nRun: ${test.name}`);
+    test.closure.apply(test);
   }
-  
+
   var tend = Date.now();
   var dur = tend-tstart;
 
@@ -59,56 +105,11 @@ Test.run = function(verbose=false, ion=false, baseline=false) {
 
   if(Test.failed.size!=0) {
     print(`\n${Test.failed.size} tests failed.`);
-    for(var test of Test.failed) print(`\n***\n*** Failed (given:${test.given}, expect:${test.expect}) @ ${test.predicate}`);
+    for(var test of Test.failed) print(`\n*** Failed: ${test.name}: (given:${test.given}, expect:${test.expect}) @ ${test.predicate}`);
   }
 
   if(Test.skipped.size!=0) {
     print(`\n${Test.skipped.size} tests skipped.`);
-    for(var test of Test.skipped) print(`\n****\n*** Skipped @ ${test.predicate}`);
+    for(var test of Test.skipped) print(`\n**** Skipped ${test.name}: ${test.predicate}`);
   }
-}
-
-/**
- * Function toBe 
- **/
-Test.toBe = function(skip, value) {
-
-  if(skip) {
-    Test.skipped.add({predicate:this});
-    return true;
-  } 
-
-  var result = this.apply();
-
-  if(result===value) {
-    Test.passed.add({predicate:this});
-    return true;
-  } else {
-    Test.failed.add({predicate:this, expect:value, given:result});
-    return true;
-  }
-}
-
-/**
- * Function expect
- **/
-Test.expect = function (predicate, skip=false) {
-  Object.defineProperty(predicate, "toBe", {
-    value: Test.toBe.bind(predicate, skip), writable: false, configurable: false,
-  });
-  return predicate;
-}
-
-/**
- * Function expect true
- **/
-Test.expectTrue = function (predicate, skip=false) {
-  return Test.toBe.call(predicate, skip, true);
-}
-
-/**
- * Function expect false
- **/
-Test.expectFalse = function (predicate, skip=false) {
-  return Test.toBe.call(predicate, skip, false);
 }
